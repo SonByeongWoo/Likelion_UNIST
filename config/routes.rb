b8314ddb4b65add1cce8c5fe293d22c85ec12a3b
Son <sbw@plurry.net>
2016-01-14 14:29:26 +0900

    controller home about service member qna 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/service'
+
+  get 'static_pages/member'
+
+  get 'static_pages/qna'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
